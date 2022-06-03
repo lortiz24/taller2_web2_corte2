2f74fa05294d328dfd30aa5972de97f5f0b192d8
unknown <ortizfrancoluisjavier@gmail.com>
2022-06-03 17:04:45 -0500

    correcion importacion

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import ContenedorAnimes from './ContenedorAnimes'
+
 import ContenedorResultados from './ContenedorResultados'
 import InputName from './InputName'
 
